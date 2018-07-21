aca05f1d5bfc3b0bc77ed19110ab33b724e3f4d7
benich2194 <benich2194@gmail.com>
2018-07-21 09:20:48 +0300

    i understand now

--- iLeague/src/Controller/MainClass.java
+++ iLeague/src/Controller/MainClass.java
@@ -6,7 +6,6 @@ class MainClass
 {
     public static void main(String args[]){
         SysData.getInstance().addAdmin("admin@walla.com", "admin");
-        System.out.println("checking");
         java.awt.EventQueue.invokeLater(() -> {
             new Login().setVisible(true);
         });    
