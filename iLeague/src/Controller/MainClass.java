cd734d7868e3aa2614dd9aff36d1ef8ba8402d74
benich2194 <benich2194@gmail.com>
2018-07-21 09:18:56 +0300

    added something to check

--- iLeague/src/Controller/MainClass.java
+++ iLeague/src/Controller/MainClass.java
@@ -6,6 +6,7 @@ class MainClass
 {
     public static void main(String args[]){
         SysData.getInstance().addAdmin("admin@walla.com", "admin");
+        System.out.println("checking");
         java.awt.EventQueue.invokeLater(() -> {
             new Login().setVisible(true);
         });    
